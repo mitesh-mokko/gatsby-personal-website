08d9f0a899891fbb28378b7c9444239cbee9b41c
Mitesh Shah <mitesh@mokko.io>
2020-06-18 18:59:10 -0500

    Cleanup continues.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,49 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 
 import Layout from "../components/Layout";
-import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
+//import Features from "../components/Features";
+//import BlogRoll from "../components/BlogRoll";
 import { HTMLContent } from "../components/Content";
 
 export const IndexPageTemplate = ({ image, title, heading, html }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          height: "150px",
-          lineHeight: "1",
-          justifyContent: "space-around",
-          alignItems: "left",
-          flexDirection: "column",
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+    {/*
+    <section className="section">
+      <div className="container">
+        <div
+          className="full-width-image margin-top-0"
           style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em",
+            backgroundImage: `url(${
+              !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+            backgroundPosition: `top left`,
+            backgroundAttachment: `fixed`,
           }}
         >
-          {title}
-        </h1>
+          <div
+            style={{
+              display: "flex",
+              height: "150px",
+              lineHeight: "1",
+              justifyContent: "space-around",
+              alignItems: "left",
+              flexDirection: "column",
+            }}
+          >
+            <h1
+              className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+              style={{
+                boxShadow:
+                  "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+                backgroundColor: "rgb(255, 68, 0)",
+                color: "white",
+                lineHeight: "1",
+                padding: "0.25em",
+              }}
+            >
+              {title}
+            </h1>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+    */}
+
     <section className="section section--gradient">
       <div className="container">
         <HTMLContent content={html} />
