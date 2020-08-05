e2ceb6d3861ee0735f9303ade3d077eefedc9c98
Mitesh Shah <mitesh@mokko.io>
2020-08-05 16:08:07 -0500

    Greatly enhanced options for these tiles.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -12,22 +12,30 @@ const techStack = [
   {
     id: "1",
     name: "React",
+    bg: "#61dafb",
+    color: "#222",
   },
   {
     id: "2",
     name: "React Native",
+    bg: "#61dafb",
+    color: "#222",
   },
   {
     id: "3",
-    name: "Node",
+    name: "Node.js",
+    bg: "#026e00",
   },
   {
     id: "4",
     name: "MongoDB",
+    bg: "#13aa52",
   },
   {
     id: "5",
     name: "Express",
+    bg: "#eeeeee",
+    color: "black",
   },
 ];
 
@@ -35,7 +43,8 @@ const CoolCardContainer = styled.div`
   position: relative;
   border-radius: 1rem;
   padding: 1rem;
-  background-color: #fff;
+  background-color: ${(props) => props.bg || "#48e"};
+  color: ${(props) => props.color || "white"};
   height: 8rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
   display: flex;
@@ -45,6 +54,7 @@ const CoolCardContainer = styled.div`
   font-weight: bold;
   transition: box-shadow 1s ease, transform 1s ease;
   transform-style: preserve-3d;
+  //  overflow: hidden;
 
   &:hover {
     box-shadow: 0 8px 20px rgba(0, 0, 0, 0.1);
@@ -55,11 +65,35 @@ const CoolCardContainer = styled.div`
 
 const CoolCardContent = styled.div`
   position: relative;
-  transform: perspective(100px) translateZ(30px);
+  transform: perspective(100px) translateZ(12px);
+  pointer-events: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 `;
 
-const CoolCard = ({ label }) => {
+const Shine = styled.div`
+  position: absolute;
+  opacity: 0.25;
+  background: radial-gradient(white, transparent 50%);
+  border-radius: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+  transform: translateZ(1px);
+
+  ${(props) =>
+    props.offset
+      ? `transform: translateX(${props.offset.x * 100}px) translateY(${
+          props.offset.y * 60
+        }px) translateZ(1px) scale3d(2, 2, 2)`
+      : null};
+`;
+
+const CoolCard = ({ label, bg, color }) => {
   const [mousePos, setMousePos] = useState(null);
+  const enableShine = false;
   const xRange = 12;
   const yRange = 20;
 
@@ -78,18 +112,26 @@ const CoolCard = ({ label }) => {
               2 -
             1.0;
 
-        setMousePos({ x: event_offsetX, y: event_offsetY });
+        setMousePos({
+          x: event_offsetX,
+          y: event_offsetY,
+          width: currentTargetRect.width,
+          height: currentTargetRect.height,
+        });
       }}
+      bg={bg}
+      color={color}
       isMouseOver={!!mousePos}
       style={{
         transform: !!mousePos
           ? `rotateY(${-mousePos.x * yRange}deg) rotateX(${
               mousePos.y * xRange
-            }deg) scale3d(1.06, 1.06, 1.06)`
+            }deg) perspective(100px) scale3d(1.06, 1.06, 1.06)`
           : null,
       }}
     >
       <CoolCardContent>{label}</CoolCardContent>
+      {enableShine && !!mousePos && <Shine offset={mousePos} />}
     </CoolCardContainer>
   );
 };
@@ -104,7 +146,7 @@ export const IndexPageTemplate = ({ image, title, heading, html }) => (
         <div className="columns is-multiline">
           {techStack.map((t) => (
             <div key={t.id} className="column is-4">
-              <CoolCard label={t.name} />
+              <CoolCard label={t.name} color={t.color} bg={t.bg} />
             </div>
           ))}
         </div>
