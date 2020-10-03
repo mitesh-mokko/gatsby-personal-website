efbe8edff07e9d157e04e134e305a7b9b089aadf
Mitesh Shah <mitesh@mokko.io>
2020-10-02 23:24:31 -0500

    Fantastic - the website is easier to make now!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,62 +1,90 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { MDXProvider } from '@mdx-js/react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.scss'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
-import { ThemeProvider, LightTheme, LightGlobalStyle } from 'piano-ui'
+import {
+  ThemeProvider,
+  LightTheme,
+  LightGlobalStyle,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Paragraph
+} from 'piano-ui'
+
+const currentTheme = LightTheme
+const currentGlobalStyle = LightGlobalStyle
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
-    <ThemeProvider theme={LightTheme}>
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{title}</title>
-          <meta name="description" content={description} />
+    <ThemeProvider theme={currentTheme}>
+      <currentGlobalStyle />
+      <MDXProvider
+        components={{
+          h1: H1,
+          h2: H2,
+          h3: H3,
+          h4: H4,
+          h5: H5,
+          h6: H6,
+          p: Paragraph
+        }}
+      >
+        <div>
+          <Helmet>
+            <html lang="en" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href={`${withPrefix('/')}img/apple-touch-icon.png`}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href={`${withPrefix('/')}img/favicon-32x32.png`}
-            sizes="32x32"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href={`${withPrefix('/')}img/favicon-16x16.png`}
-            sizes="16x16"
-          />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href={`${withPrefix('/')}img/apple-touch-icon.png`}
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              href={`${withPrefix('/')}img/favicon-32x32.png`}
+              sizes="32x32"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              href={`${withPrefix('/')}img/favicon-16x16.png`}
+              sizes="16x16"
+            />
 
-          <link
-            rel="mask-icon"
-            href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-            color="#ff4400"
-          />
-          <meta name="theme-color" content="#fff" />
+            <link
+              rel="mask-icon"
+              href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+              color="#ff4400"
+            />
+            <meta name="theme-color" content="#fff" />
 
-          <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={title} />
-          <meta property="og:url" content="/" />
-          <meta
-            property="og:image"
-            content={`${withPrefix('/')}img/og-image-lower.jpg`}
-          />
-        </Helmet>
-        <Navbar />
-        <section className="section">
-          <div className="container">{children}</div>
-        </section>
+            <meta property="og:type" content="business.business" />
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content="/" />
+            <meta
+              property="og:image"
+              content={`${withPrefix('/')}img/og-image-lower.jpg`}
+            />
+          </Helmet>
+          <Navbar />
+          <section className="section">
+            <div className="container">{children}</div>
+          </section>
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
+      </MDXProvider>
     </ThemeProvider>
   )
 }
